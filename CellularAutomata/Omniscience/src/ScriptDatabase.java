4a736588ea0b9358d4ffc48dc08009885ef9a459
InfiniteSearchSpace <ben.kraakman.2011@gmail.com>
2014-09-01 09:55:59 +1200

    Added video-bitmap capture, added secondary cell override function

---
+++ CellularAutomata/Omniscience/src/ScriptDatabase.java
@@ -0,0 +1,26 @@
+
+public class ScriptDatabase {
+	public ScriptDatabase() {
+		//load existing dattabases of functions / parameters
+		
+	}
+	
+	public void capture_current_script(){
+		//grab base functions
+		//grab neighbourhoods
+		//grab other configs
+		config_rule();
+		//write to existing datbase
+		
+		
+	}
+	
+	public void load_database() {
+		//add all scripts in database file to possible selected rules, and combinations 
+	}
+	
+	private void config_rule() {
+		//get name and general descreiption of rule from input boxes
+	}
+	
+}
